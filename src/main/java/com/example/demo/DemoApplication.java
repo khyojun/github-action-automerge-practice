9c7ec4059fb40b530f46979f1b6b391c4aee57c3
Khyojun <nandong5923@naver.com>
2024-12-05 18:18:08 +0900

    command fix

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,29 @@
 package com.example.demo;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.servlet.ApplicationContextRequestMatcher;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class DemoApplication {
     //
+
+    @Bean
+    ApplicationRunner applicationRunner(){
+        return args ->{
+            HashMap<String, Integer> map = new HashMap<>();
+            map.entrySet().stream().
+                    sorted(Comparator.comparing(Map.Entry::getValue, Comparator.reverseOrder())).
+        };
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
